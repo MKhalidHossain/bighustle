77094cc7e4599b3595c555826cf2682b1eead9e5
MD. KHALID HOSSAIN <101824816+MKhalidHossain@users.noreply.github.com>
2025-12-30 12:37:57 +0600

    Dev khalid (#6)
    * profile section all screens are done.
    
    * dd
    
    * ticket section are done as well.
    
    * License screens , License alerts , edit licencse info are uptodate
    
    * fix up radio button

--- lib/moduls/ticket/presentation/screen/ticket_screen.dart
+++ lib/moduls/ticket/presentation/screen/ticket_screen.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
 import '../../../../core/constants/app_routes.dart';
+import '../widget/payment_method_dialog.dart';
 import '../widget/ticket_card.dart';
 import '../widget/ticket_summary_card.dart';
 
-class TicketScreen extends StatelessWidget {
+class TicketScreen extends StatefulWidget {
   const TicketScreen({super.key});
 
   @override
+  State<TicketScreen> createState() => _TicketScreenState();
+}
+
+class _TicketScreenState extends State<TicketScreen> {
+  @override
   Widget build(BuildContext context) {
+    bool _isPaypalselected = false;
     return Scaffold(
       backgroundColor: const Color(0xFFF2F2F2),
       body: SafeArea(
@@ -39,12 +46,39 @@ class TicketScreen extends StatelessWidget {
               type: 'Speeding',
               amount: r'$250.00',
               dueDate: 'Nov 25,2025',
-              onPayNow: () => Navigator.of(context).pushNamed(
-                AppRoutes.planPricing,
+              onPayNow: () => showDialog(
+                context: context,
+                barrierDismissible: true,
+                builder: (dialogContext) { 
+                  bool selected = _isPaypalselected;
+                  return StatefulBuilder(
+                    builder: (context,setDialogState){
+                      return PaymentMethodDialog(
+                     // local state for dialog UI
+                    isSelected: _isPaypalselected,
+                    
+                    onSelectChanged: (val) {
+                      _isPaypalselected = val;
+                      // setDialogState(() => _isPaypalselected = !_isPaypalselected);
+                      setState(() => _isPaypalselected = !_isPaypalselected);
+                    },
+                        
+                    onClose: () => Navigator.of(context).pop(dialogContext),
+                    onPay: () => Navigator.of(
+                      context,
+                    ).pushNamed(AppRoutes.planPricingDetails),
+                                    );
+                    },
+                   
+                  );
+                }
               ),
-              onViewDetails: () => Navigator.of(context).pushNamed(
-                AppRoutes.ticketDetails,
-              ),
+
+              // Navigator.of(context).pushNamed(
+              //   AppRoutes.planPricing,
+              // ),
+              onViewDetails: () =>
+                  Navigator.of(context).pushNamed(AppRoutes.ticketDetails),
             ),
             const SizedBox(height: 16),
             const Text(
@@ -58,9 +92,8 @@ class TicketScreen extends StatelessWidget {
               amount: r'$250.00',
               dueDate: 'Nov 25,2025',
               isPaid: true,
-              onViewDetails: () => Navigator.of(context).pushNamed(
-                AppRoutes.ticketDetails,
-              ),
+              onViewDetails: () =>
+                  Navigator.of(context).pushNamed(AppRoutes.ticketDetails),
             ),
           ],
         ),
