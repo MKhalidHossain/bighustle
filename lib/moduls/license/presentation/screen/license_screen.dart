db849980d105424d7c4fdb87f2989311cf7820af
MKhalidHossain <compilefiller@gmail.com>
2025-12-31 09:55:16 +0600

    fix up licence screen

--- lib/moduls/license/presentation/screen/license_screen.dart
+++ lib/moduls/license/presentation/screen/license_screen.dart
@@ -16,6 +16,7 @@ class LicenseScreen extends StatelessWidget {
         backgroundColor: const Color(0xFFF2F2F2),
         elevation: 0,
         centerTitle: true,
+        automaticallyImplyLeading: false,
         title: const Text(
           'License',
           style: TextStyle(fontWeight: FontWeight.w600, color: Colors.black87),
