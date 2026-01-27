dd45eda60e42661edb7d84c3b7468d7be20362b7
MKhalidHossain <compilefiller@gmail.com>
2026-01-27 12:29:15 +0600

    Remove unused imports from internal_service_di.dart

--- lib/core/di/internal_service_di.dart
+++ lib/core/di/internal_service_di.dart
@@ -1,19 +1,14 @@
 import 'package:get/get.dart';
-
 import '../../moduls/auth/implement/auth_interface_impl.dart';
 import '../../moduls/auth/interface/auth_interface.dart';
 import '../../moduls/home/implement/home_interface_impl.dart';
 import '../../moduls/home/interface/home_interface.dart';
-import '../../moduls/home/implement/teen_driver_experience_interface_impl.dart';
-import '../../moduls/home/interface/teen_driver_experience_interface.dart';
 import '../../moduls/license/implement/license_interface_impl.dart';
 import '../../moduls/license/interface/license_interface.dart';
 import '../../moduls/profile/implement/profile_interface_impl.dart';
 import '../../moduls/profile/interface/profile_interface.dart';
 import '../../moduls/ticket/implement/ticket_interface_impl.dart';
 import '../../moduls/ticket/interface/ticket_interface.dart';
-import '../../moduls/notification/implement/notification_interface_impl.dart';
-import '../../moduls/notification/interface/notification_interface.dart';
 import '../services/app_pigeon/app_pigeon.dart';
 
 void initServices() {
