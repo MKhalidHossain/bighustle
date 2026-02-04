7609dc6350b7e149a1dd932e6d2880d7c5bc6672
MKhalidHossain <compilefiller@gmail.com>
2026-02-04 09:43:27 +0600

    urls change to live

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -208,8 +208,10 @@ class _LocalHostWifi {
   static const String baseUrl = 'http://10.10.5.85:5000/api/v1';
 }
 class _LiveHostUrls {
-  static const String socketUrl = 'https://backend-bigghustle-icpx.onrender.com';
-   static const String baseUrl = 'https://backend-bigghustle-icpx.onrender.com/api/v1';
+  // static const String socketUrl = 'https://backend-bigghustle-icpx.onrender.com';
+  //  static const String baseUrl = 'https://backend-bigghustle-icpx.onrender.com/api/v1';
+   static const String socketUrl = 'https://api.drivestatusllc.com';
+   static const String baseUrl = 'https://api.drivestatusllc.com/api/v1';
   //static const String baseUrl = 'http://10.10.5.85:5000/api/v1';
 }
 
