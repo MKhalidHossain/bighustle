8e946d588f2d2c4ac787a7c5ee3e6057df2c2e2c
MKhalidHossain <compilefiller@gmail.com>
2026-02-04 09:40:38 +0600

    make url live

---
+++ Bighustle/BighustleTests/BighustleTests.swift
@@ -0,0 +1,16 @@
+//
+//  BighustleTests.swift
+//  BighustleTests
+//
+//  Created by SAA on 2/2/26.
+//
+
+import Testing
+
+struct BighustleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
